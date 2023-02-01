49f3128fac032dd762cc07c63d4e6564117e4d77
dmytrocash <dmitrycash1932@gmail.com>
2023-02-01 19:12:36 +0200

    Adding unit tests for helper functions.

--- test_utils.py
+++ test_utils.py
@@ -6,19 +6,6 @@ from utils import validate_post_body, validate_signup_body, get_password_hash, \
 
 class PositiveTestCase(unittest.TestCase):
 
-    def test_get_password_hash(self):
-        self.assertEqual(get_password_hash(
-            '12345678'), '$2b$12$LxwRsweKXFfzmu/vk4S8AOuKeqhhrBhul3SMuZwbtHLjrhmscikai')
-
-    def test_encode_user_data(self):
-        self.assertEqual(encode_user_data(1, 'developmentkey'),
-                         {'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.EE70sW0n7H-TnN9jMYF1BHkCzmLknK4JYMdlSK6ISEA'})
-
-    def test_decode_user_token(self):
-        self.assertEqual(decode_user_token(
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.EE70sW0n7H-TnN9jMYF1BHkCzmLknK4JYMdlSK6ISEA',
-            'developmentkey'), {'id': 1})
-
     def test_validate_signup_body(self):
         self.assertEqual(
             validate_signup_body({'email': '1@example.com', 'password': '12345678'}),
